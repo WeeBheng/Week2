bc48bd5b56142cbdb780b825b78547e2d8a1e37f
Koh Wee Bheng <22016788@myrp.edu.sg>
2023-04-27 11:43:25 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22016788/week2/MainActivity.kt
@@ -0,0 +1,11 @@
+package sg.edu.rp.c346.id22016788.week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
